0efb5a13dd70321c24e68462c37eb68fb2062fc0
marcosChalet <chaletmarcos@gmail.com>
2023-04-22 08:04:37 -0300

    Mudando favicon

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,55 +10,67 @@ import Skills from '@/components/Skills';
 import { BarContext } from '@/context/BarContext';
 import TrackVisibility from 'react-on-screen';
 import { useContext } from 'react';
+import Head from 'next/head';
 
 export default function Home() {
   const bar = useContext(BarContext);
 
   return (
-    <main className="scrollbar-hide relative bg-slate-600">
-      <Ribons section={bar.section} />
-      <TrackVisibility>
-        {({ isVisible }: { isVisible: boolean }) => (
-          <Hero isVisible={isVisible} setSection={bar.setSection} />
-        )}
-      </TrackVisibility>
+    <>
+      <Head>
+        <title>Portfólio</title>
+        <meta
+          name="description"
+          content="Página pessoal para contato e exposição da minha experiência profissional."
+        />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicn.ico" />
+      </Head>
+      <main className="scrollbar-hide relative bg-slate-600">
+        <Ribons section={bar.section} />
+        <TrackVisibility>
+          {({ isVisible }: { isVisible: boolean }) => (
+            <Hero isVisible={isVisible} setSection={bar.setSection} />
+          )}
+        </TrackVisibility>
 
-      <TrackVisibility>
-        {({ isVisible }: { isVisible: boolean }) => (
-          <About isVisible={isVisible} setSection={bar.setSection} />
-        )}
-      </TrackVisibility>
+        <TrackVisibility>
+          {({ isVisible }: { isVisible: boolean }) => (
+            <About isVisible={isVisible} setSection={bar.setSection} />
+          )}
+        </TrackVisibility>
 
-      <TrackVisibility>
-        {({ isVisible }: { isVisible: boolean }) => (
-          <Skills isVisible={isVisible} setSection={bar.setSection} />
-        )}
-      </TrackVisibility>
+        <TrackVisibility>
+          {({ isVisible }: { isVisible: boolean }) => (
+            <Skills isVisible={isVisible} setSection={bar.setSection} />
+          )}
+        </TrackVisibility>
 
-      <TrackVisibility>
-        {({ isVisible }: { isVisible: boolean }) => (
-          <Learning isVisible={isVisible} setSection={bar.setSection} />
-        )}
-      </TrackVisibility>
+        <TrackVisibility>
+          {({ isVisible }: { isVisible: boolean }) => (
+            <Learning isVisible={isVisible} setSection={bar.setSection} />
+          )}
+        </TrackVisibility>
 
-      <TrackVisibility>
-        {({ isVisible }: { isVisible: boolean }) => (
-          <Experiences isVisible={isVisible} setSection={bar.setSection} />
-        )}
-      </TrackVisibility>
+        <TrackVisibility>
+          {({ isVisible }: { isVisible: boolean }) => (
+            <Experiences isVisible={isVisible} setSection={bar.setSection} />
+          )}
+        </TrackVisibility>
 
-      <TrackVisibility offset={500}>
-        {({ isVisible }: { isVisible: boolean }) => (
-          <Projects isVisible={isVisible} setSection={bar.setSection} />
-        )}
-      </TrackVisibility>
+        <TrackVisibility offset={500}>
+          {({ isVisible }: { isVisible: boolean }) => (
+            <Projects isVisible={isVisible} setSection={bar.setSection} />
+          )}
+        </TrackVisibility>
 
-      <TrackVisibility>
-        {({ isVisible }: { isVisible: boolean }) => (
-          <Contact isVisible={isVisible} setSection={bar.setSection} />
-        )}
-      </TrackVisibility>
-      <Footer />
-    </main>
+        <TrackVisibility>
+          {({ isVisible }: { isVisible: boolean }) => (
+            <Contact isVisible={isVisible} setSection={bar.setSection} />
+          )}
+        </TrackVisibility>
+        <Footer />
+      </main>
+    </>
   );
 }
